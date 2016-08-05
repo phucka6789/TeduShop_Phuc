72604a4c53f3a4dfc15c3932189fbed1a29ce9bc
phucka6789 <tdphuc10@gmail.com>
2016-08-05 22:47:06 +0700

    This Frist Commit

---
+++ Tedu.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tedu.Common
+{
+    public class Class1
+    {
+    }
+}
