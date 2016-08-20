6f8ea965c09955f277646bae2525954bf168d3ac
phucka6789 <tdphuc10@gmail.com>
2016-08-20 23:14:51 +0700

    Bai 19 Angular

--- TeduShop.Web/Scripts/spa/APP.js
+++ TeduShop.Web/Scripts/spa/APP.js
@@ -1,21 +1,45 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 var myApp = angular.module('myModule', []);
 //myApp.controller("myController", myController);
-
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
-
 myApp.controller("schoolController", schoolController);
-
-//declare
-function studentController($rootScope,$scope) {
-    //$rootScope.message = "this is my message from studentController";
-}
-function teacherController($scope) {
-     $scope.message = "this is my message from teacherController";
-}
-
-function schoolController($scope) {
-    $scope.message = "this is my message from schoolController";
-}
+myApp.service('Validator',Validator)
+//myApp.controller("studentController", studentController);
+//myApp.controller("teacherController", teacherController);
+////declare
+//function studentController($rootScope,$scope) {
+//    //$rootScope.message = "this is my message from studentController";
+//}
+//function teacherController($scope) {
+//     $scope.message = "this is my message from teacherController";
+//}
 // lớp cha (schoolController) khai báo  thì lớp con (teacherController,studentController) có thể được kế thừa và có thể overider được giống trong hướng đối tượng
+
+
+schoolController.$inject = ['$scope', 'Validator']
+function schoolController($scope,Validator) {
+    // $scope.message = "this is my message from schoolController";
+   // $scope.message = Validator.CheckNumber(2);
+    $scope.checkNumber = function ()
+    {
+        $scope.message = Validator.CheckNumber(2);
+    }
+    $scope.num = 1;
+}
+
+function Validator($window)
+{
+    return {
+        CheckNumber:CheckNumber
+    }
+    function CheckNumber(input)
+    {
+        if(input % 2 ==0)
+        {
+            // $window.alert('this is even ');
+            return 'This is even';
+        }
+        else
+            //$window.alert('this is odd ');
+            return 'This is odd';
+    }
+}
