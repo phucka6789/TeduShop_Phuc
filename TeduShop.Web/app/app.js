72d1d6d88c66054fc6fcb4a771c9320840e3805d
phucka6789 <tdphuc10@gmail.com>
2016-08-29 22:15:17 +0700

    Bài 37: Tạo Form Đăng nhập cho người Quản trị

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,21 +1,31 @@
 ﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
 
 (function () {
-    angular.module('tedushop', [
-        'tedushop.products',
-        'tedushop.product_categories',
-        'tedushop.common'
-    ]).config(config);
+    angular.module('tedushop',
+        ['tedushop.products',
+         'tedushop.product_categories',
+         'tedushop.common'])
+        .config(config);
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: "/admin",
-            templateUrl: "/app/components/home/homeView.html",
-            controller: "homeController"
-        });
-        // nếu không tìm thấy thì sẽ chuyển sang /admin
-        $urlRouterProvider.otherwise('/admin');
+        $stateProvider
+            .state('base', {
+                url: '',
+                templateUrl: '/app/shared/views/baseView.html',
+                abstract: true
+            }).state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent: 'base',
+                templateUrl: "/app/components/home/homeView.html",
+                controller: "homeController"
+            });
+        $urlRouterProvider.otherwise('/login');
     }
 })();
